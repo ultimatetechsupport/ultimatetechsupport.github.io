644519145c20227cccc052c2abf701f8ec3cccdf
ultimatetechsupport <ultimatetechsupportteam@gmail.com>
2026-06-30 16:48:14 -0400

    Modify JS

---
+++ assets/js/googletag.js
@@ -0,0 +1,9 @@
+async src = "https://www.googletagmanager.com/gtag/js?id=G-XW7Q3GKJGT"
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-XW7Q3GKJGT');
