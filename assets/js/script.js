fdfbbe5bca8b975bd1db927a65c995ce14d3c699
ultimatetechsupport <ultimatetechsupportteam@gmail.com>
2026-06-30 16:17:42 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 window.onload = function() {
     document.getElementById("url-display").innerHTML = document.URL;
 };
+
+// Get the full URL
+const fullUrl = document.URL;
+
+// Remove https:// and keep www.
+const cleanUrl = fullUrl.replace(/^https?:\/\//, '').replace(/^www\./, '');
+
+// Display in the page
+document.getElementById('currentUrl').textContent = `https://www.${cleanUrl}`;
