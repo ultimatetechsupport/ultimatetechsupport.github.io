75c03d8f2b1b45fb7921b1468ab2f6515dbc2896
ultimatetechsupport <ultimatetechsupportteam@gmail.com>
2026-07-26 19:19:58 -0400

    Add Close Btn

--- assets/js/light-mode-message.js
+++ assets/js/light-mode-message.js
@@ -2,3 +2,10 @@ const isLight = window.matchMedia('(prefers-color-scheme: light)').matches;
 if (isLight) {
   document.getElementById('message-banner').style.display = 'block';
 }
+
+const closeButton = document.getElementById('closeBtn');
+const targetDiv = document.getElementById('myDiv');
+
+closeButton.addEventListener('click', () => {
+  targetDiv.style.display = 'none'; 
+});
