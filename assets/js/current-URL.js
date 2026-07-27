3bb39927341064188a076c9ead9605f51d3f9ee4
ultimatetechsupport <ultimatetechsupportteam@gmail.com>
2026-07-26 20:05:06 -0400

    BUGS

--- assets/js/current-URL.js
+++ assets/js/current-URL.js
@@ -6,7 +6,7 @@ window.onload = function() {
 const fullUrl = document.URL;
 
 // Remove https:// and keep www.
-const cleanUrl = fullUrl.replace(/^https?:\/\//, '').replace(/^www\./, '');
+const cleanUrl = fullUrl.replace(/^https?:\/\//, '');
 
 // Display in the page
 document.getElementById('currentUrl').textContent = `www.${cleanUrl}`;
